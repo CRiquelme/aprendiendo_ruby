f2d967a0b31c4fac9c7ed76202d1dda777132e46
Carlos Riquelme <criquelme2@gmail.com>
2022-06-24 00:37:10 -0300

    The print method

---
+++ the_print_method.rb
@@ -0,0 +1,16 @@
+puts "Hello"
+puts "World"
+# Resultado:
+# Hello
+# World
+
+# print hace una copia de puts pero no imprime el salto de línea
+print "Hello"
+print "World"
+# Resultado: HelloWorld
+
+# Una alternativa para generar un espacio es agregar un nuevo print con una cadena vacía
+print "Hello"
+print " "
+print "World"
+# Resultado: Hello World
