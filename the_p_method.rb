88b7a1fc7312c40e68869bfbfa30b0d6574ada92
Carlos Riquelme <criquelme2@gmail.com>
2022-06-24 00:45:46 -0300

    The p method

---
+++ the_p_method.rb
@@ -0,0 +1,20 @@
+puts "Steven Seagal"
+# resultado: Steven Seagal
+
+# p entrega información que no entrega puts
+p "Steven Seagal"
+# resultado: "Steven Seagal", nos dice que es un string
+
+puts "Hi there, this is a 
+big line break"
+# resultado: Hi there, this is a
+# big line break
+
+p "Hi there, this is a
+big line break"
+# resultado: "Hi there, this is a\nbig line break"
+# \n es un salto de línea
+
+puts 5 # resultado: 5
+p 5 # resultado: 5
+# Son números por lo que no vemos diferencias visibles
