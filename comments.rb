fd52752ecc2329e38bab281885f249503a2e05e5
Carlos Riquelme <criquelme2@gmail.com>
2022-06-24 17:04:34 -0300

    Comments

---
+++ comments.rb
@@ -0,0 +1,11 @@
+# This is a comment
+
+# print "Hello World"
+puts "Hello World" # Resultado: Hello World
+
+# Multi-line comment
+=begin
+  This is a multi-line comment
+  This is a multi-line comment
+  This is a multi-line comment
+=end
