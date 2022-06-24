d83093bf13f2d0848ecbda0b6db17a8c93f77eb7
Carlos Riquelme <criquelme2@gmail.com>
2022-06-24 13:04:26 -0300

    Ejercicio 01 operaciones aritméticas

---
+++ ejercicio_01.rb
@@ -0,0 +1,29 @@
+# 1. What does the statement puts "Hello" do?
+# Answer: Prints "Hello" to the screen with a line break
+
+# 2. What does the statement print "Ruby is fun!" do?
+# Answer: Prints "Ruby is fun!" to the screen with a line break
+
+# 3. What does the statement p "Elephant" do?
+# Answer: Prints "Elephant" to the screen as a string with double quotes and without a line break
+
+# 4. What Ruby symbol / operator is used to multiply numeric values?
+# Answer: The * symbol
+
+# 5. What Ruby symbol / operator is used to subtract numeric values?
+# Answer: The - symbol
+
+# 6. What Ruby symbol / operator is used to divide numeric values?
+# Answer: The / symbol
+
+# 7. What Ruby symbol / operator is used to add numeric values?
+# Answer: The + symbol
+
+# 8. What happens when the addition operator ( + ) is used with two strings?
+# Answer: The strings are concatenated
+
+# 9. What is the name of the % operator?
+# Answer: Modulo
+
+# 10. What is the evaluation of 23 % 4?
+# Answer: 3
