2598dbb1f7d9b7cf54691582f770ea4fabb87733
Carlos Riquelme <criquelme2@gmail.com>
2022-06-24 00:26:27 -0300

    first commit

---
+++ the_puts_method.rb
@@ -0,0 +1,25 @@
+puts "Hello world"
+puts "I'm alive and well!"
+
+# El punto y coma se utiliza cuando hay más de una sentencia en la misma línea
+# Ejemplo:
+puts "Hello world"; puts "I'm alive and well!"
+
+puts "I bought 5 dollars at the store."
+puts "I made $65 dollars or a profit of 25%!"
+
+puts 5
+puts 3.14159
+
+# Puedo hacer operaciones matemáticas
+puts 5 + 3
+# Ruby no soporta operaciones matemáticas con strings
+# puts "5" + 3 # error de coerción
+
+# Para hacer concatenación
+puts "5" + "3" # "53"
+
+# Agregar un puts vacío puede ser una salto de línea
+puts
+puts
+puts "See, there's a bunch of empty lines!"
