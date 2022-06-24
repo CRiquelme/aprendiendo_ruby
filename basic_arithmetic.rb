6be5ee3b364901afac68287f80defc826642a22e
Carlos Riquelme <criquelme2@gmail.com>
2022-06-24 12:56:16 -0300

    Basic arithmetic

---
+++ basic_arithmetic.rb
@@ -0,0 +1,23 @@
+# Operación de suma
+p 1 + 5   # 6
+
+# Operación de resta
+p 1 - 5   # -4
+
+# Operación de multiplicación
+p 1 * 5   # 5
+
+# Operación de división
+p 10 / 5  # 2
+p 1 / 5   # 0 (no es exacta)
+p 1.0 / 5 # 0.2 (uno o los dos deben ser float)
+
+# Operación de exponenciación
+p 2 ** 3  # 8
+
+# Operación de modulo
+p 10 % 3  # 1
+p 6 % 3   # 0
+
+# Concatenación de cadenas
+p "Hola" + " mundo" # "Hola mundo"
