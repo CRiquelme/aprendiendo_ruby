4b2a59ff5226ea6afbfaf91914ecca740573fbbf
Carlos Riquelme <criquelme2@gmail.com>
2022-06-25 21:22:54 -0300

    Intro to boolean

---
+++ Numbers and boolean/intro_to_boolean.rb
@@ -0,0 +1,11 @@
+p 1 < 2   #=> true
+p 1 > 2   #=> false
+p 1 <= 2  #=> true
+p 1 >= 2  #=> false
+p 1 == 2  #=> false
+p 1 != 2  #=> true
+# <=> https://stackoverflow.com/a/827656/1128702
+p 1 <=> 2 #=> -1
+p 2 <=> 2 #=> 0
+p 2 <=> 1 #=> 1
+
