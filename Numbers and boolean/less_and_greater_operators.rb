c544d4006ac5833c38aa39f56cf4c1c40aec0ff9
Carlos Riquelme <criquelme2@gmail.com>
2022-06-26 18:05:18 -0300

    Less and greater operators

---
+++ Numbers and boolean/less_and_greater_operators.rb
@@ -0,0 +1,15 @@
+# less operator is <
+# greater operator is >
+
+p 5 < 10  #=> true
+p 5 > 10  #=> false
+p 5 < 5   #=> false
+p 5 > 5   #=> false
+
+# less or equal operator is <=
+# greater or equal operator is >=
+p 1 <= 2  #=> true
+p 1 >= 2  #=> false
+p 1 <= 1  #=> true
+p 1 >= 1  #=> true
+
