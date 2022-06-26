7d77bb90b31461178f611f70cce0c0f06a66e376
Carlos Riquelme <criquelme2@gmail.com>
2022-06-26 18:51:09 -0300

    Assigment operators

---
+++ Numbers and boolean/assigment_operators.rb
@@ -0,0 +1,29 @@
+# addition
+a = 10
+a = a + 10    # a += 10 is the same as a = a + 10
+p a           #=> 20
+
+# subtraction
+b = 10
+b -= 2        # b -= 2 is the same as b = b - 2
+p b           #=> 8
+
+# multiplication
+c = 10
+c *= 2        # c *= 2 is the same as c = c * 2
+p c           #=> 20
+
+# division
+d = 10
+d /= 2        # d /= 2 is the same as d = d / 2
+p d           #=> 5
+
+# modulus
+e = 10
+e %= 2        # e %= 2 is the same as e = e % 2
+p e           #=> 0
+
+# exponentiation
+f = 10
+f **= 2       # f **= 2 is the same as f = f ** 2
+p f           #=> 100
