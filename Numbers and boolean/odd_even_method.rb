f1767063ad727de79676faead86413befde64bde
Carlos Riquelme <criquelme2@gmail.com>
2022-06-25 23:40:32 -0300

    odd? and even? method

---
+++ Numbers and boolean/odd_even_method.rb
@@ -0,0 +1,14 @@
+# .odd?
+p 10.odd? #=> false
+p 11.odd? #=> true
+p 12.odd? #=> false
+p 13.odd? #=> true
+
+# .even?
+p 10.even? #=> true
+p 11.even? #=> false
+p 12.even? #=> true
+p 13.even? #=> false
+
+# .odd? and .even? are methods that are defined in the Integer class
+# Doesn't work with strings or floats
