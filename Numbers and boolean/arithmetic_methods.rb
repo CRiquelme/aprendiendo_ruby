13e8358eca9c57934ac24454469df1d7414583db
Carlos Riquelme <criquelme2@gmail.com>
2022-06-26 18:15:17 -0300

    Arithmetic methods

---
+++ Numbers and boolean/arithmetic_methods.rb
@@ -0,0 +1,21 @@
+# addition operator is +
+# subtraction operator is -
+# multiplication operator is *
+# division operator is /
+# modulus operator is %
+# exponent operator is **
+# .next operator
+p 5.next      #=> 6
+# and optional is use .next(n)
+p 5.next()    #=> 6
+
+p 3 * 3       #=> 9
+p 3.* 3       #=> 9
+
+p 3 / 3       #=> 1
+p 3./ 3       #=> 1
+p 3.div(3)    #=> 1
+
+p 3 % 3       #=> 0
+p 3.% 3       #=> 0
+p 3.modulo(3) #=> 0
